ab240ef60dead51531dfb33232b4be96ddf84ffe
AlexanderTitilin <lambdadelta89@gmail.com>
2023-04-22 17:19:40 +0300

    Добавлено воспроизведение звука

--- audioplayer.py
+++ audioplayer.py
@@ -8,10 +8,15 @@ class Audioplayer(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.currentFiles = []
+        self.setWindowTitle("Audioplayer")
         self.currentTrackIndex = 0
         self.mediaPlayer = QtMultimedia.QMediaPlayer()
+        self.audioOutput = QtMultimedia.QAudioOutput()
+        self.mediaPlayer.setAudioOutput(self.audioOutput)
+        self.audioOutput.setVolume(50)
         self.listTrack = QtWidgets.QListWidget()
-        self.buttonGet = QtWidgets.QPushButton("pause")
+        self.buttonPause = QtWidgets.QPushButton("pause")
+        self.buttonPlay = QtWidgets.QPushButton("play")
         self.buttonPrev = QtWidgets.QPushButton("<")
         self.buttonNext = QtWidgets.QPushButton(">")
         self.buttonFiles = QtWidgets.QPushButton("files")
@@ -20,13 +25,16 @@ class Audioplayer(QtWidgets.QWidget):
         self.currentTrack = QtWidgets.QLabel()
         buttonLayout = QtWidgets.QHBoxLayout()
         buttonLayout.addWidget(self.buttonPrev)
-        buttonLayout.addWidget(self.buttonGet)
+        buttonLayout.addWidget(self.buttonPause)
+        buttonLayout.addWidget(self.buttonPlay)
         buttonLayout.addWidget(self.buttonNext)
         buttonLayout.addWidget(self.buttonFiles)
         buttonLayout.addWidget(self.progressBar)
         self.buttonFiles.clicked.connect(self.getFiles)
         self.buttonNext.clicked.connect(lambda: self.nextTrack(1))
         self.buttonPrev.clicked.connect(lambda: self.nextTrack(-1))
+        self.buttonPause.clicked.connect(self.pause)
+        self.buttonPlay.clicked.connect(self.play)
         self.listTrack.itemDoubleClicked.connect(self.dummy)
         self.baseLayout.addLayout(buttonLayout)
         self.baseLayout.addWidget(self.currentTrack)
@@ -45,12 +53,15 @@ class Audioplayer(QtWidgets.QWidget):
                                         files[0]))
         for file in self.currentFiles:
             self.listTrack.addItem(file)
-            print(self.listTrack.count())
 
     @QtCore.Slot()
     def dummy(self):
         self.currentTrack.setText(self.listTrack.currentItem().text())
+        path = QtCore.QUrl.fromLocalFile(self.listTrack.currentItem().text())
         self.currentTrackIndex = self.listTrack.currentRow()
+        self.mediaPlayer.setSource(path)
+        print(self.mediaPlayer.source())
+        self.mediaPlayer.play()
 
     @QtCore.Slot()
     def nextTrack(self, i):
@@ -58,11 +69,21 @@ class Audioplayer(QtWidgets.QWidget):
                                   i) % self.listTrack.count()
         self.listTrack.setCurrentRow(self.currentTrackIndex)
         self.currentTrack.setText(self.listTrack.currentItem().text())
+        self.mediaPlayer.stop()
+        self.dummy()
+
+    @QtCore.Slot()
+    def pause(self):
+        self.mediaPlayer.pause()
+
+    @QtCore.Slot()
+    def play(self):
+        self.mediaPlayer.play()
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     widget = Audioplayer()
-    widget.resize(800, 400)
+    # widget.resize(800, 400)
     widget.show()
     sys.exit(app.exec())
